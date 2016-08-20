a720a663b12226e45679edd7a3acfb356fb1d1cb
Taylor Wu <Taylorwu21@gmail.com>
2016-08-20 17:17:21 -0600

    finished jquery logic

--- app/assets/javascripts/channels/memory.js
+++ app/assets/javascripts/channels/memory.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
-	let count = 0
-	let choiceOne
-	let choiceTwo
+	let count = 0;
+	let choiceOne;
+	let choiceTwo;
+	let card = 1;
+	let tries = 1;
+	let shuffle = []
+
+	$('.shuffle').on('click', $(this), function() {
+		
+	})
+
+
   $('.card').on('click', $(this), function() {
   	function flipBack() {
+			let numberOfTries = $('.tries').text(tries);
+			let randomFailedToast = ['NOPE', 'SORRY', 'TOO BAD', 'TRY AGAIN', 'GET BETTER'];
+			let rand = randomFailedToast[Math.floor(Math.random() * randomFailedToast.length)];
+  		Materialize.toast(rand, 4000);
+  		$(choiceOne.id).addClass('card');
+  		$(choiceTwo.id).addClass('card');
+  		count = 0;
+  		tries ++;
   	}
   	count ++;
   	if(count > 2) {
@@ -11,12 +28,25 @@ $(document).ready(function() {
   	}
   	$(this).removeClass('card');
   	if(count === 1) {
-  		let something = $(this).html();
-  		alert(something);
   		choiceOne = {
-  			value: $(this),
-  			id: $(this).attr('id')
+  			value: $(this).attr("class"),
+  			id: $(this).attr("id", card)
   		};
+  	}
+  	else if(count === 2) {
+  		choiceTwo = {
+  			value: $(this).attr("class"),
+  			id: $(this).attr("id", card)
+  		};
+  		if(choiceOne.value === choiceTwo.value) {
+  			let randomToast = ['NICE!', 'GOOD JOB!', 'SICK BRO', 'TIGHT']
+  			let rand = randomToast[Math.floor(Math.random() * randomToast.length)];
+  			Materialize.toast(rand, 4000);
+  			count = 0;
+  		}
+  		else {
+  			setTimeout(flipBack, 2000);
+  		}
   	}
   });
 });
