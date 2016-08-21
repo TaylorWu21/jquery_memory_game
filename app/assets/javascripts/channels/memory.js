ddad8e96a234c255f17865aa061a4f278859f193
Taylor Wu <Taylorwu21@gmail.com>
2016-08-20 21:17:32 -0600

    finished shuffle

--- app/assets/javascripts/channels/memory.js
+++ app/assets/javascripts/channels/memory.js
@@ -4,17 +4,60 @@ $(document).ready(function() {
 	let choiceTwo;
 	let card = 1;
 	let tries = 1;
-	let shuffle = []
+	let wowClass = ['death_knight', 'death_knight',
+									'demon_hunter', 'demon_hunter',
+									'druid', 'druid',
+									'hunter', 'hunter',
+									'mage', 'mage',
+									'monk', 'monk',
+									'paladin', 'paladin',
+									'priest', 'priest',
+									'rogue', 'rogue',
+									'shaman', 'shaman',
+									'warlock', 'warlock',
+									'warrior', 'warrior']
 
-	$('.shuffle').on('click', $(this), function() {
-		
+	// Array Shuffler
+	function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+	}
+
+	// Starts a new game
+	$('.new_game').on('click', $(this), function() {
+		tries = 1;
+		$('.image').addClass('card');
 	})
 
+	// Starts Game
+	function newGame() {
+		let mixedCards = shuffle(wowClass);
+		$(mixedCards).each(function(index, value) {
+			$('.add_cards').append("<div class='image col s2 card " + value +"'></div>");
+			console.log(value);
+		});
+	}
 
+	newGame();
+
+	// Game Logic
   $('.card').on('click', $(this), function() {
   	function flipBack() {
 			let numberOfTries = $('.tries').text(tries);
-			let randomFailedToast = ['NOPE', 'SORRY', 'TOO BAD', 'TRY AGAIN', 'GET BETTER'];
+			let randomFailedToast = ['NOPE', 'SORRY', 'TOO BAD', 'TRY AGAIN', 'GET BETTER', 'RANDY YOU SUCK', 'NHI YOU SUCK'];
 			let rand = randomFailedToast[Math.floor(Math.random() * randomFailedToast.length)];
   		Materialize.toast(rand, 4000);
   		$(choiceOne.id).addClass('card');
@@ -39,7 +82,7 @@ $(document).ready(function() {
   			id: $(this).attr("id", card)
   		};
   		if(choiceOne.value === choiceTwo.value) {
-  			let randomToast = ['NICE!', 'GOOD JOB!', 'SICK BRO', 'TIGHT']
+  			let randomToast = ['NICE!', 'GOOD JOB!', 'NICE PICK', 'BRIT IS THE BEST']
   			let rand = randomToast[Math.floor(Math.random() * randomToast.length)];
   			Materialize.toast(rand, 4000);
   			count = 0;
